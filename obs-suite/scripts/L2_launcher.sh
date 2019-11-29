55eaf890ec32e70ddc41512f4f366986b74c48ac
Irene Perez Gonzalez <iregon@jasmin-sci1.ceda.ac.uk>
2019-11-29 15:06:41 +0000

    Remove level2 list to call to main.py

--- obs-suite/scripts/L2_launcher.sh
+++ obs-suite/scripts/L2_launcher.sh
@@ -12,10 +12,8 @@
 # Main paths are set sourcing r092009_setenv0.sh
 #
 # sid_deck_list_file is: sid-dck yyyy-mm yyyy-mm
-# level2list is a json file initialized by L2_list_create.py and edited to needs,
-# per sid-dck info on what to include on level2.
 #
-# Usage: ./L2_launcher.sh release update source_dataset list_file level2list
+# Usage: ./L2_launcher.sh release update source_dataset list_file
 
 # Get JOB
 function nk_jobid {
@@ -30,7 +28,6 @@ release=$1
 update=$2
 source=$3
 sid_deck_list_file=$4
-level2list=$5
 
 ffs="-"
 level=level2
@@ -63,7 +60,7 @@ do
   echo "Setting deck L1e scratch directory: $sid_deck_scratch_dir"
   rm -rf $sid_deck_scratch_dir;mkdir -p $sid_deck_scratch_dir
 
-  jobid_cp=$(nk_jobid bsub -J $sid_deck'L2c' -oo $sid_deck_scratch_dir/"L2_copy.o" -eo $sid_deck_scratch_dir/"L2_copy.e" -q short-serial -W $job_time_cp -M $job_memo_cp -R "rusage[mem=$job_memo_cp]" python $scripts_directory/L2_main.py $data_directory $sid_deck $release $update $source $level2list)
+  jobid_cp=$(nk_jobid bsub -J $sid_deck'L2c' -oo $sid_deck_scratch_dir/"L2_copy.o" -eo $sid_deck_scratch_dir/"L2_copy.e" -q short-serial -W $job_time_cp -M $job_memo_cp -R "rusage[mem=$job_memo_cp]" python $scripts_directory/L2_main.py $data_directory $sid_deck $release $update $source)
   jobid_io=$(nk_jobid bsub -J $sid_deck'L2io' -w "done($jobid_cp)" -oo $sid_deck_scratch_dir/"L2_io.e" -eo $sid_deck_scratch_dir/"L2_io.e" -q short-serial -W $job_time_io -M $job_memo_io -R "rusage[mem=$job_memo_io]" python $code_directory/reports/report_io.py $data_directory $release $update $source $level $sid_deck $y_init $y_end)
   bsub -J $sid_deck'L2REP' -w "done($jobid_io)" -oo $sid_deck_scratch_dir/"L2_rep.e" -eo $sid_deck_scratch_dir/"L2_rep.e" -q short-serial -W $job_time_rep -M $job_memo_rep -R "rusage[mem=$job_memo_rep]" python $code_directory/reports/pdf_report_jinja_$level.py $data_directory $release $update $source $sid_deck $y_init $y_end
 
