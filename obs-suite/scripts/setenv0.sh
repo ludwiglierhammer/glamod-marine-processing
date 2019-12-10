11a52a17c75b5336271953451902fb47761072a6
Irene Perez Gonzalez <iregon@jasmin-sci1.ceda.ac.uk>
2019-12-10 13:38:05 +0000

    Changed home directories

--- obs-suite/scripts/setenv0.sh
+++ obs-suite/scripts/setenv0.sh
@@ -1,18 +1,19 @@
-home_directory=/gws/nopw/j04/c3s311a_lot2/
-home_directory_smf=/gws/smf/j04/c3s311a_lot2/
-code_directory=$home_directory/code/marine_code/glamod-marine-processing/obs-suite
-code_directory_smf=$home_directory_smf/code/marine_code/glamod-marine-processing/obs-suite
-data_directory=$home_directory/data/marine
+home_directory=/group_workspaces/jasmin2/glamod_marine
+home_directory_smf=/gws/smf/j04/c3s311a_lot2
+code_directory=$home_directory_smf/code/marine_code/glamod-marine-processing/obs-suite
 pyTools_directory=$code_directory/modules/python
 scripts_directory=$code_directory/scripts
-pyEnvironment_directory=$code_directory_smf/pyenvs/env0
+pyEnvironment_directory=$code_directory/pyenvs/env0
+data_directory=$home_directory/data
+
+echo 'Data directory is '$data_directory
+echo 'Code directory is '$code_directory
 
 # Activate python environment and add jaspy3.7 path to LD_LIBRARY_PATH so that cartopy and other can find the geos library
-echo "Python environment loaded from gws: $pyEnvironment_directory"
 source $pyEnvironment_directory/bin/activate
 export PYTHONPATH="$pyTools_directory:${PYTHONPATH}"
 export LD_LIBRARY_PATH=/apps/contrib/jaspy/miniconda_envs/jaspy3.7/m3-4.5.11/envs/jaspy3.7-m3-4.5.11-r20181219/lib/:$LD_LIBRARY_PATH
-
+echo "Python environment loaded from gws: $pyEnvironment_directory"
 # Create the scratch directory for the user
 scratch_directory=/work/scratch-nompiio/$(whoami)
 if [ ! -d $scratch_directory ]
