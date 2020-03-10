c87a6934d91d1d922803d1f071a033687b744140
Irene Perez Gonzalez <iregon@jasmin-sci1.ceda.ac.uk>
2020-03-10 13:40:21 +0000

    Adapted to current test Release

--- obs-suite/compare_migration.sh
+++ obs-suite/compare_migration.sh
@@ -4,16 +4,22 @@ level=$1
 sd=$2
 table=$3
 
-rMigration=/gws/nopw/j04/c3s311a_lot2/data/marine/rMigration/ICOADS_R3.0.0T/$level/$sd
-r092019=/gws/nopw/j04/c3s311a_lot2/data/marine/r092019/ICOADS_R3.0.0T/$level/$sd
+ddata=/group_workspaces/jasmin2/glamod_marine/data
+rMigration=$ddata/Release2/ICOADS_R3.0.0T/$level/$sd
+r092019=$ddata/r092019/ICOADS_R3.0.0T/$level/$sd
 
 for file in $(ls $rMigration/$table*)
 do
-	echo 'Comparing...'
 	base1=$(basename $file)
-        echo $rMigration/$base1
-	base2=$(basename $base1 rMigration-000000.psv)'r092019-000000.psv'
-	echo $r092019/$base2
-	diff <(cut -d \| -f 1-37 $rMigration/$base1) <(cut -d \| -f 1-37 $r092019/$base2)
+	base2=$(basename $base1 Release2-tests.psv)'r092019-000000.psv'
+        if [ $table == 'header' ]
+        then
+                echo $base1 'VS'  $base2': comparing header table in 2 chunks'
+		diff <(cut -d \| -f 1-37 $rMigration/$base1) <(cut -d \| -f 1-37 $r092019/$base2)
+        	diff <(cut -d"|" -f40- $rMigration/$base1) <(cut -d"|" -f40- $r092019/$base2)
+        else
+		echo $base1 'VS'  $base2
+		diff $rMigration/$base1 $r092019/$base2
+        fi
 done
 
