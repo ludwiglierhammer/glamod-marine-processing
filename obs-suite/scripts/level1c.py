ee5cddd9b6d091707c78a8094a8f644856918c2f
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-03-16 12:34:30 +0000

    Bug in dict key solved

--- obs-suite/scripts/level1c.py
+++ obs-suite/scripts/level1c.py
@@ -147,7 +147,7 @@ def read_table_files(table):
     leak_pattern = filename_field_sep.join([table,fileID,'????' + filename_field_sep + '??.psv'])
     leak_files = glob.glob(os.path.join(prev_level_path,leak_pattern))
     if len(leak_files)>0:
-        validation_dict[table]['leak_in'] = 0
+        validation_dict[table]['leaks_in'] = 0
         for leak_file in leak_files:
             logging.info('Reading datetime leak file {}'.format(leak_file))
             file_base = os.path.splitext(os.path.basename(leak_file))[0]
