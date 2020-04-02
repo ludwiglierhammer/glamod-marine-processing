a3f9f52f46ea51cc4065c67879842bba6b39142b
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-04-02 21:23:10 +0100

    New config management

--- obs-suite/scripts/process_array_output_hdlr.py
+++ obs-suite/scripts/process_array_output_hdlr.py
@@ -15,41 +15,39 @@ import sys
 import json
 import shutil
 
-print(sys.path)
-
 FFS='-'
 
 # GET INPUT ARGUMENTS
-LSF_output_path = sys.argv[1] #/scratch-nompiio/<user_id>/  <release> <update> <dataset> <process> <data_level> <sid-dck> status
-data_path = sys.argv[2] #/scratch-nompiio/<user_id>/  <release> <update> <dataset> <process> <data_level> <sid-dck> status
-release = sys.argv[3]
-update = sys.argv[4]
-dataset = sys.argv[5]
-process = sys.argv[6]
-data_level = sys.argv[7]
-sid_dck = sys.argv[8]
-exit_status = sys.argv[9]
-rm_input = sys.argv[10]
-
-#GET LSF ARRAY INDEX AND FILES
 LSF_IDX = os.environ['LSB_JOBINDEX']
-LSF_output_file = os.path.join(LSF_output_path,release,dataset,process,sid_dck,str(LSF_IDX) + '.o')
-LSF_input_file = os.path.join(LSF_output_path,release,dataset,process,sid_dck,str(LSF_IDX) + '.input')
+configfile =  sys.argv[1]
+exit_status = sys.argv[2]
+rm_input = sys.argv[3]
 
-with open(LSF_input_file,'r') as fO:
-    job_config = json.load(fO)
+LSF_IDX = os.path.basename(configfile).split('.')[0]
+LSF_output_path = os.path.dirname(configfile)
+LSF_output_file = os.path.join(LSF_output_path,LSF_IDX + '.o')
 
-yyyy = job_config.get('yyyy')
-mm = job_config.get('mm')
+# GET INFO FROM CONFIG FILE
+with open(configfile,'r') as fO:
+    config = json.load(fO) 
+    
+data_path = config.get('data_directory')
+release = config.get('release')
+update = config.get('udpate')
+dataset = config.get('dataset')
+data_level = config.get('job_config').get('data_level')
+sid_dck = config.get('sid_dck')
+yyyy = config.get('yyyy')
+mm = config.get('mm')
 
-job_log_dir = os.path.join(data_path,release,dataset,data_level,'log',sid_dck)
+# RENAME AND MOVE LSF OUTFILE
 ext='ok' if exit_status == '0' else 'failed'
+log_dir = os.path.join(data_path,release,dataset,data_level,'log',sid_dck)
 job_log_file = FFS.join([str(yyyy),str(mm),release,update]) + '.' + ext
-
-job_log =  os.path.join(job_log_dir,job_log_file)
-
+job_log =  os.path.join(log_dir,job_log_file)
 shutil.copy(LSF_output_file,job_log)
 os.remove(LSF_output_file)
-# Consider removing *.input files when this is working....
-if rm_input == '0':
-    os.remove(LSF_input_file)
+
+# REMVE *.input FILE
+#if rm_input == '0':
+#    os.remove(configfile)
