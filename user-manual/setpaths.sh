e59cf55bec6236a9b76c3696f5488b66bf6e22e0
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-05-19 14:56:15 +0100

    Adpated to user-manual

--- user-manual/setpaths.sh
+++ user-manual/setpaths.sh
@@ -1,12 +1,14 @@
 home_directory=/group_workspaces/jasmin2/glamod_marine
+data_directory=$home_directory/data
+
 home_directory_smf=/gws/smf/j04/c3s311a_lot2
-code_directory=$home_directory_smf/code/marine_code/glamod-marine-processing_ipg/obs-suite
-pyTools_directory=$code_directory/modules/python
-scripts_directory=$code_directory/scripts
+code_repo_directory=$home_directory_smf/code/marine_code/glamod-marine-processing_ipg
+pyTools_directory=$code_repo_directory/obs-suite/modules/python
+um_code_directory=$code_repo_directory/user-manual
 data_directory=$home_directory/data
 
 echo 'Data directory is '$data_directory
-echo 'Code directory is '$code_directory
+echo 'User manual code directory is '$um_code_directory
 
 # Create the scratch directory for the user
 scratch_directory=/work/scratch-nompiio/$(whoami)
