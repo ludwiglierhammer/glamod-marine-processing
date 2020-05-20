713850f24b1bbd3e14347585693cc12d1fba7e19
Irene Perez Gonzalez <iregon@jasmin-sci1.ceda.ac.uk>
2020-05-20 23:34:09 +0100

    Manage no jobs for a sd_dk

--- user-manual/data_summaries/sd_monthly_grids_launcher.sh
+++ user-manual/data_summaries/sd_monthly_grids_launcher.sh
@@ -36,6 +36,13 @@ for sid_dck in $(awk '{print $1}' $process_list_file)
 do
    sid_dck_log_dir=$out_dir/log/$sid_dck
    arrl=$(ls -1q $sid_dck_log_dir/*-$run_id".input" | wc -l)
+   
+   if [[ "$arrl" == '0' ]]
+   then
+        echo 'No jobs found for $sid_dck'
+   	continue
+   fi
+
 
    jobid=$(nk_jobid bsub -J $sid_dck"[1-$arrl]" -oo $sid_dck_log_dir/"%I-"$run_id".o" -eo $sid_dck_log_dir/"%I-"$run_id".o" -q short-serial -W $job_time_hhmm -M $job_memo_mbi -R "rusage[mem=$job_memo_mbi]" python $pyscript $sid_dck_log_dir/\$LSB_JOBINDEX"-"$run_id".input")
 
