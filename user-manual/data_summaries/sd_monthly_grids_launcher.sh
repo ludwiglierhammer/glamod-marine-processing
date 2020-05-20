2cc6fa82dc44131def7e0168a9dac001cbed16c8
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-05-20 22:29:51 +0100

    Added LSF output hdlr

--- user-manual/data_summaries/sd_monthly_grids_launcher.sh
+++ user-manual/data_summaries/sd_monthly_grids_launcher.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
+#. FUNCTIONS -------------------------------------------------------------------
+# Get JOB
+function nk_jobid {
+    output=$($*)
+    echo $output | head -n1 | cut -d'<' -f2 | cut -d'>' -f1
+}
+# ------------------------------------------------------------------------------
 source ../setpaths.sh
 source ../setenv0.sh
 
@@ -17,6 +23,7 @@ failed_only=$7
 
 pyconfig=$um_code_directory/data_summaries/sd_monthly_grids_config_array.py
 pyscript=$um_code_directory/data_summaries/sd_monthly_grids.py
+pyhdlr=$um_code_directory/data_summaries/process_array_output_hdlr.py
 run_id=$(basename $script_config_file .json)
 out_dir=$data_directory/user_manual/release_summaries/$release/$dataset
 
@@ -29,8 +36,13 @@ for sid_dck in $(awk '{print $1}' $process_list_file)
 do
    sid_dck_log_dir=$out_dir/log/$sid_dck
    arrl=$(ls -1q $sid_dck_log_dir/*-$run_id".input" | wc -l)
-   echo $sid_dck"[1-$arrl]"
-   echo $sid_dck_log_dir/"%I-"$run_id".o"
-   echo $sid_dck_log_dir/\$LSB_JOBINDEX"-"$run_id".input"
-   bsub -J $sid_dck"[1-$arrl]" -oo $sid_dck_log_dir/"%I-"$run_id".o" -eo $sid_dck_log_dir/"%I-"$run_id".o" -q short-serial -W $job_time_hhmm -M $job_memo_mbi -R "rusage[mem=$job_memo_mbi]" python $pyscript $sid_dck_log_dir/\$LSB_JOBINDEX"-"$run_id".input"
+
+   jobid=$(nk_jobid bsub -J $sid_dck"[1-$arrl]" -oo $sid_dck_log_dir/"%I-"$run_id".o" -eo $sid_dck_log_dir/"%I-"$run_id".o" -q short-serial -W $job_time_hhmm -M $job_memo_mbi -R "rusage[mem=$job_memo_mbi]" python $pyscript $sid_dck_log_dir/\$LSB_JOBINDEX"-"$run_id".input")
+
+   bsub -J OK"[1-$arrl]" -w "done($jobid[*])" -oo $sid_dck_log_dir/"%I-"$run_id".ho" -eo $sid_dck_log_dir/"%I-"$run_id".ho" -q short-serial -W 00:01 -M 10 -R "rusage[mem=10]" \
+   python $pyhdlr $sid_dck_log_dir/\$LSB_JOBINDEX"-"$run_id".input" 0 1
+
+   bsub -J OK"[1-$arrl]" -w "exit($jobid[*])" -oo $sid_dck_log_dir/"%I-"$run_id".ho" -eo $sid_dck_log_dir/"%I-"$run_id".ho" -q short-serial -W 00:01 -M 10 -R "rusage[mem=10]" \
+   python $pyhdlr $sid_dck_log_dir/\$LSB_JOBINDEX"-"$run_id".input" 1 1
+
 done
