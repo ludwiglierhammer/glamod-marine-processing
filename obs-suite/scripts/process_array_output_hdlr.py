ea9d50063e071acafd07f35822a3b41f909390d6
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-06-01 14:37:33 +0100

    New LSF input output file path

--- obs-suite/scripts/process_array_output_hdlr.py
+++ obs-suite/scripts/process_array_output_hdlr.py
@@ -13,41 +13,33 @@ This is a temporary script file.
 import os
 import sys
 import json
-import shutil
 
 FFS='-'
 
 # GET INPUT ARGUMENTS
-LSF_IDX = os.environ['LSB_JOBINDEX']
-configfile =  sys.argv[1]
+LSF_input_file =  sys.argv[1]
 exit_status = sys.argv[2]
 rm_input = sys.argv[3]
 
-LSF_IDX = os.path.basename(configfile).split('.')[0]
-LSF_output_path = os.path.dirname(configfile)
+LSF_IDX = os.path.basename(LSF_input_file).split('.')[0]
+LSF_output_path = os.path.dirname(LSF_input_file)
 LSF_output_file = os.path.join(LSF_output_path,LSF_IDX + '.o')
 
 # GET INFO FROM CONFIG FILE
-with open(configfile,'r') as fO:
-    config = json.load(fO) 
+with open(LSF_input_file,'r') as fO:
+    config = json.load(fO)
 
-data_path = config.get('data_directory')
 release = config.get('release')
 update = config.get('update')
-dataset = config.get('dataset')
-data_level = config.get('job_config').get('data_level')
-sid_dck = config.get('sid_dck')
 yyyy = config.get('yyyy')
 mm = config.get('mm')
 
 # RENAME AND MOVE LSF OUTFILE
 ext='ok' if exit_status == '0' else 'failed'
-log_dir = os.path.join(data_path,release,dataset,data_level,'log',sid_dck)
 job_log_file = FFS.join([str(yyyy),str(mm),release,update]) + '.' + ext
-job_log =  os.path.join(log_dir,job_log_file)
-shutil.copy(LSF_output_file,job_log)
-os.remove(LSF_output_file)
+job_log =  os.path.join(LSF_output_path,job_log_file)
+os.rename(LSF_output_file,job_log)
 
 # REMOVE *.input FILE
 if rm_input == '0':
-    os.remove(configfile)
+    os.remove(LSF_input_file)
