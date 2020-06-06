586d1c5838316a009980770db11e338951830c07
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-06-06 23:23:03 +0100

    First commit

---
+++ user-manual/data_summaries/mg_merge_period_sd_interrelease_launcher.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#. FUNCTIONS -------------------------------------------------------------------
+# Get JOB
+function nk_jobid {
+    output=$($*)
+    echo $output | head -n1 | cut -d'<' -f2 | cut -d'>' -f1
+}
+# ------------------------------------------------------------------------------
+source ../setpaths.sh
+source ../setenv0.sh
+
+# Here make sure we are using fully expanded paths, as some may be passed to a config file
+log_dir=$1
+script_config_file=$(readlink --canonicalize  $2)
+process_list_file=$(readlink --canonicalize  $3)
+failed_only=$4
+
+pyconfig=$um_code_directory/data_summaries/mg_merge_period_sd_interrelease_config.py
+pyscript=$um_code_directory/data_summaries/mg_merge_period.py
+pyhdlr=$um_code_directory/data_summaries/process_output_hdlr.py
+run_id=$(basename $script_config_file .json)
+
+job_time_hhmm=02:00
+job_memo_mbi=8000
+
+
+python $pyconfig $log_dir $script_config_file $process_list_file $failed_only
+
+for sid_dck in $(awk '{print $1}' $process_list_file)
+do
+   sid_dck_log_dir=$log_dir/$sid_dck
+   arrl=$(ls -1q $sid_dck_log_dir/$run_id".input" 2> /dev/null | wc -l)
+
+   if [[ "$arrl" == '0' ]]
+   then
+        echo "No jobs found for $sid_dck"
+   	continue
+   else
+        echo "Launching $sid_dck array"
+   fi
+
+   jobid=$(nk_jobid bsub -J $sid_dck -oo $sid_dck_log_dir/$run_id".o" -eo $sid_dck_log_dir/$run_id".o" -q short-serial -W $job_time_hhmm -M $job_memo_mbi -R "rusage[mem=$job_memo_mbi]" python $pyscript $sid_dck_log_dir/$run_id".input")
+
+   bsub -J OK -w "done($jobid)" -oo $sid_dck_log_dir/$run_id".ho" -eo $sid_dck_log_dir/$run_id".ho" -q short-serial -W 00:01 -M 10 -R "rusage[mem=10]" \
+   python $pyhdlr $sid_dck_log_dir/$run_id".o" 0 0
+
+   bsub -J ER -w "exit($jobid)" -oo $sid_dck_log_dir/$run_id".ho" -eo $sid_dck_log_dir/$run_id".ho" -q short-serial -W 00:01 -M 10 -R "rusage[mem=10]" \
+   python $pyhdlr $sid_dck_log_dir/$run_id".o" 1 0
+done
