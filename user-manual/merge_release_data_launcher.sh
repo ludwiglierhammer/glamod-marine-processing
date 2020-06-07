ffa292c6f384e4142a8b0b4a0d41eb8e8d805035
Irene Perez Gonzalez <iregon@jasmin-sci1.ceda.ac.uk>
2020-06-07 15:42:56 +0100

    Added some logging

--- user-manual/merge_release_data_launcher.sh
+++ user-manual/merge_release_data_launcher.sh
@@ -14,13 +14,14 @@ source setenv0.sh
 um_version=$1
 config_file=$(readlink --canonicalize  $2)
 process_list_file=$(readlink --canonicalize  $3)
-failed_only=$4
 
 pyscript="$um_code_directory/merge_release_data.py $data_directory $um_version $config_file"
 pyhdlr=$um_code_directory/process_output_hdlr.py
 run_id=$(basename $0 .sh)
 log_dir=$data_directory"/user_manual/"$um_version"/log/merge_release_data"
 if [ ! -d $log_dir ]; then mkdir -p $log_dir; fi
+
+echo "LOG DIR IS $log_dir"
 
 job_time_hhmm=00:30
 job_memo_mbi=500
@@ -30,6 +31,7 @@ do
    sid_dck_log_dir=$log_dir/$sid_dck
    if [ ! -d $sid_dck_log_dir ]; then mkdir -p $sid_dck_log_dir; fi
 
+   echo $sid_dck
    jobid=$(nk_jobid bsub -J $sid_dck -oo $sid_dck_log_dir/$run_id".o" -eo $sid_dck_log_dir/$run_id".o" -q short-serial -W $job_time_hhmm -M $job_memo_mbi -R "rusage[mem=$job_memo_mbi]" python $pyscript $sid_dck)
 
    bsub -J OK -w "done($jobid)" -oo $sid_dck_log_dir/$run_id".ho" -eo $sid_dck_log_dir/$run_id".ho" -q short-serial -W 00:01 -M 10 -R "rusage[mem=10]" \
