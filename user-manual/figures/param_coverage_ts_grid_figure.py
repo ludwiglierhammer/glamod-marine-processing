39b24e0913a4635a65cc33c28814e74b15984bab
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-06-14 15:11:34 +0100

    First commit

---
+++ user-manual/figures/param_coverage_ts_grid_figure.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 11:43:22 2018
+
+@author: iregon
+"""
+import os
+import sys
+import logging
+import matplotlib.pyplot as plt
+#plt.switch_backend('agg')
+import datetime
+import itertools
+import xarray as xr
+
+
+from figures import var_properties
+
+logging.getLogger('plt').setLevel(logging.INFO)
+logging.getLogger('mpl').setLevel(logging.INFO)
+
+# PARAMS ----------------------------------------------------------------------
+# Set plotting defaults
+plt.rc('legend',**{'fontsize':12})        # controls default text sizes
+plt.rc('axes', titlesize=12)     # fontsize of the axes title
+plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=10)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=10)    # fontsize of the tick labels
+plt.rc('figure', titlesize=14)  # fontsize of the figure title
+# END PARAMS ------------------------------------------------------------------
+
+def flip(items, ncol):
+    return itertools.chain(*[items[i::ncol] for i in range(ncol)])
+
+if __name__ == "__main__":
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+#    dir_data = sys.argv[1]
+#    dir_out = sys.argv[2]
+#    
+    dir_data = '/Users/iregou/C3S/data'
+    dir_out = dir_data
+    file_in_id = '-no_reports_grid_ts-optimal.nc'
+    file_out = 'params_no_reports_coverage_ts.pdf'
+    start = datetime.datetime(1851,1,1)
+    stop = datetime.datetime(2010,12,31)
+    
+    
+    filtered = True
+    log_scale_cells = False
+    log_scale_reports = True
+    n_reports_color = 'FireBrick'
+    n_cells_color = 'Black'
+        
+    observation_tables = ['observations-at','observations-sst','observations-dpt',
+                          'observations-slp','observations-ws','observations-wd']   
+    table = 'header'
+    file_pattern = table + file_in_id
+    hdr_dataset = xr.open_dataset(os.path.join(dir_data,file_pattern))
+    header_n_cells = hdr_dataset['counts'].where(hdr_dataset['counts'] > 0).count(dim=['longitude','latitude'])
+    header_n_reports = hdr_dataset['counts'].sum(dim=['longitude','latitude'])
+    if filtered:
+        header_n_cells = header_n_cells.rolling(time=12, center=True).mean()
+        header_n_reports = header_n_reports.rolling(time=12, center=True).mean()
+        
+    
+    f, ax = plt.subplots(3, 2, figsize=(10,10),sharex=True,sharey=True)# 
+    ax2 = ax.copy()
+    for i,table in enumerate(observation_tables):
+        var = table.split('-')[1]
+        title = var_properties['long_name_upper'][var]
+        c = 0 if i%2 == 0 else 1
+        r = int(i/2)
+        file_pattern = table + file_in_id
+        dataset = xr.open_dataset(os.path.join(dir_data,file_pattern))
+        n_cells = dataset['counts'].where(dataset['counts'] > 0).count(dim=['longitude','latitude'])
+        n_reports = dataset['counts'].sum(dim=['longitude','latitude'])
+        if filtered:
+            n_cells = n_cells.rolling(time=12, center=True).mean()
+            n_reports = n_reports.rolling(time=12, center=True).mean()
+           
+    
+        header_n_reports.plot(ax=ax[r,c],color=n_reports_color,zorder = 1 ,label='#reports',linewidth=5,alpha=0.15)
+        n_reports.plot(ax=ax[r,c],color=n_reports_color,zorder = 3 ,label='#obs parameter')
+        ax2[r,c] = ax[r,c].twinx()
+        header_n_cells.plot(ax=ax2[r,c],color=n_cells_color,linewidth=5,alpha=0.15,zorder = 2,label='#1x1 cells reports')
+        n_cells.plot(ax=ax2[r,c],color=n_cells_color,zorder = 4, label='#1x1 cells parameter')
+        
+        ax2[r,c].set_ylabel('#1x1 cells', color=n_cells_color)
+        ax2[r,c].tick_params(axis='y', colors=n_cells_color)
+        ax[r,c].set_ylabel('#Observations', color=n_reports_color)
+        ax[r,c].tick_params(axis='y', colors=n_reports_color)
+        ax[r,c].set_title(title, color='k')
+        
+        if log_scale_reports:
+            ax[r,c].set_yscale('log')
+        if log_scale_cells:
+            ax2[r,c].set_yscale('log')
+        
+        
+        ax[r,c].tick_params(axis='x',labelbottom=True,labelrotation=0)
+        ax[r,c].tick_params(axis='y',labelleft=True,labelrotation=0)
+        ax2[r,c].ticklabel_format(axis='y', style='sci',scilimits=(-3,4))
+        ax[r,c].set_xlim([start,stop])
+        ax2[r,c].set_xlim([start,stop])
+        
+    lines, labels = ax[r,c].get_legend_handles_labels()
+    lines2, labels2 = ax2[r,c].get_legend_handles_labels()    
+    lines = [lines[1]] + [lines2[1]] + [lines[0]] + [lines2[0]]
+    labels = [labels[1]] + [labels2[1]] + [labels[0]] + [labels2[0]]
+    f.legend(flip(lines,4),flip(labels,4),loc='lower center', ncol=4)
+    f.tight_layout(rect=[0, 0.03, 1, 0.95])
+    
+    fig_path = os.path.join(dir_out,file_out)
+    plt.savefig(fig_path,bbox_inches='tight',dpi = 300)
+    plt.close(f)
---
+++ user-manual/figures/seasonal_nreports_hovmoller.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 11:43:22 2018
+
+@author: iregon
+"""
+import os
+import logging
+import matplotlib.pyplot as plt
+#plt.switch_backend('agg')
+import datetime
+import xarray as xr
+from matplotlib.colors import LogNorm
+
+from figures import var_properties
+
+
+# PARAMS ----------------------------------------------------------------------
+# Set plotting defaults
+plt.rc('legend',**{'fontsize':8})        # controls default text sizes
+plt.rc('axes', titlesize=8)     # fontsize of the axes title
+plt.rc('axes', labelsize=8)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=6)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=6)    # fontsize of the tick labels
+plt.rc('figure', titlesize=10)  # fontsize of the figure title
+figsize=(6,6)
+# END PARAMS ------------------------------------------------------------------
+
+def is_season_center(month,season):
+    if season == 'DJF':
+        return month == 1   
+    elif season == 'MAM':
+        return month == 4
+    elif season == 'JJA':
+        return month == 7
+    elif season == 'SON':
+        return month == 10
+
+if __name__ == "__main__":
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+#    dir_data = sys.argv[1]
+#    dir_out = sys.argv[2]
+    
+    dir_data = '/Users/iregou/C3S/data'
+    dir_out = dir_data
+    file_in_id = '-no_reports_grid_ts-optimal.nc'
+    file_out = 'nreports_hovmoller.png'
+    start = datetime.datetime(1851,1,1)
+    stop = datetime.datetime(2010,12,31)
+    
+    tables = ['header','observations-at','observations-sst','observations-slp',
+                      'observations-dpt','observations-wd','observations-ws']
+    
+    for table in tables:
+        if table == 'header':
+            cbar_label = '#reports'   
+        else:
+            param = table.split('-')[1]
+            cbar_label = '#Observations ' + var_properties['short_name_upper'][param]
+        
+        file_pattern = table + file_in_id
+        dataset = xr.open_dataset(os.path.join(dir_data,file_pattern))
+        
+        dataset['3_mon_counts'] = dataset['counts'].rolling(time=3, center=True).sum()
+        for season in ['DJF','MAM','JJA','SON']:
+            dataset[season] = dataset['3_mon_counts'].sel(time=is_season_center(dataset['time.month'],season)).sum(dim='longitude')
+            
+        min_counts = 1
+        if table == 'header': # Use same scale for all params
+            max_counts = dataset['3_mon_counts'].max().data.tolist()
+            normalization_f = LogNorm(vmin = min_counts,vmax = max_counts)
+        
+        f, axes = plt.subplots(nrows=2, ncols=2, figsize=figsize)
+        for i, season in enumerate(['DJF','MAM','JJA','SON']):
+            c = 0 if i%2 == 0 else 1
+            r = int(i/2)
+            dataset[season].sel(time=is_season_center(dataset['time.month'],season)).where(dataset[season]>0).plot.pcolormesh(x = 'time', y = 'latitude',vmin=min_counts,
+                       vmax=max_counts, cmap='viridis',norm = normalization_f,add_colorbar=True,
+                       extend='both',ax=axes[c,r],cbar_kwargs={'label':cbar_label})
+            axes[c,r].set_title(season)
+        f.tight_layout(rect=[0, 0.03, 1, 0.95])
+        fig_path = os.path.join(dir_out,table + '-' + file_out)
+        plt.savefig(fig_path,bbox_inches='tight',dpi = 150)
+        plt.close(f)
