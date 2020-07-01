09cb666817afe453163b430efb98e80b5d0597a4
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-07-01 22:06:44 +0100

    Moved to different repo

--- user-manual/setenv2.sh
+++
@@ -1,9 +0,0 @@
-
-# WATCH THIS HARCODING UNTIL WE DECIDE WHAT TO DO WITH REPOS AND ENVS!
-
-pyEnvironment_directory=$home_directory_smf/code/marine_code/glamod-marine-processing/obs-suite/pyenvs/env2
-# Activate python environment and add jaspy3.7 path to LD_LIBRARY_PATH so that cartopy and other can find the geos library
-source $pyEnvironment_directory/bin/activate
-export PYTHONPATH="$pyTools_directory:$um_code_directory:${PYTHONPATH}"
-export LD_LIBRARY_PATH=/apps/contrib/jaspy/miniconda_envs/jaspy3.7/m3-4.5.11/envs/jaspy3.7-m3-4.5.11-r20181219/lib/:$LD_LIBRARY_PATH
-echo "Python environment loaded from gws: $pyEnvironment_directory"
