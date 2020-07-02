f9eea5b3b3cc6ce72f04140213b2ce2a785e17ac
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-07-02 14:42:41 +0100

    No longer used

--- obs-suite/setenv1.sh
+++
@@ -1,7 +0,0 @@
-
-pyEnvironment_directory=$home_directory_smf/code/marine_code/glamod-marine-processing/obs-suite/pyenvs/env1
-# Activate python environment and add jaspy3.7 path to LD_LIBRARY_PATH so that cartopy and other can find the geos library
-source $pyEnvironment_directory/bin/activate
-export PYTHONPATH="$pyTools_directory:${PYTHONPATH}"
-export LD_LIBRARY_PATH=/apps/contrib/jaspy/miniconda_envs/jaspy3.7/m3-4.5.11/envs/jaspy3.7-m3-4.5.11-r20181219/lib/:$LD_LIBRARY_PATH
-echo "Python environment loaded from gws: $pyEnvironment_directory"
