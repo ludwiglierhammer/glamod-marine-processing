e975576be46cbcb1ee3117cadf3b6dd1ced49aea
andreas-wernecke <andreas.wernecke@posteo.net>
2023-03-06 08:56:42 +0000

    Updated path and requirements for ICHEC

--- obs-suite/setenv3.sh
+++ obs-suite/setenv3.sh
@@ -1,7 +1,9 @@
 
-pyEnvironment_directory=$home_directory_smf/code/marine_code/glamod-marine-processing/obs-suite/pyenvs/env3
+pyEnvironment_directory=$home_directory/obs-suite/pyenvs/envichec
 # Activate python environment and add jaspy3.7 path to LD_LIBRARY_PATH so that cartopy and other can find the geos library
-source $pyEnvironment_directory/bin/activate
+source activate $pyEnvironment_directory
 export PYTHONPATH="$pyTools_directory:${PYTHONPATH}"
-export LD_LIBRARY_PATH=/apps/contrib/jaspy/miniconda_envs/jaspy3.7/m3-4.5.11/envs/jaspy3.7-m3-4.5.11-r20181219/lib/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/apps/contrib/jaspy/miniconda_envs/jaspy3.7/m3-4.5.11/envs/jaspy3.7-m3-4.5.11-r20181219/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/ichec/packages/conda/2/lib/:$LD_LIBRARY_PATH
+
 echo "Python environment loaded from gws: $pyEnvironment_directory"
--- qc-suite/setenv0.sh
+++ qc-suite/setenv0.sh
@@ -1,15 +1,19 @@
-export home_directory=/gws/nopw/j04/glamod_marine
-export home_directory_smf=/gws/smf/j04/c3s311a_lot2
-export code_directory=$home_directory_smf/code/marine_code/glamod-marine-processing/qc-suite
+export home_directory=/ichec/work/glamod/glamod-marine-processing.2022
+
+#export home_directory_smf=/gws/smf/j04/c3s311a_lot2
+export code_directory=$home_directory/qc-suite
 export scripts_directory=$code_directory/scripts
 export pyEnvironment_directory=$code_directory/pyenvs/env0
 export modules_directory=$code_directory/modules
 #data_directory=$home_directory/data/datasets/ICOADS_R3.0.0T_original/
-export data_directory=$home_directory/data/datasets/ICOADS_R3.0.2T_NRT/ORIGINAL/
+#export data_directory=$home_directory/data/datasets/ICOADS_R3.0.2T_NRT/ORIGINAL/
+export data_directory=/ichec/work/glamod/data/marine/datasets/ICOADS_R3.0.2T/ORIGINAL/
 #corrections_directory=${home_directory}/data/datasets/NOC_corrections/v1x2019/
-export corrections_directory=${home_directory}/data/release_3.0.2/NOC_corrections/v1x2022/
+#export corrections_directory=${home_directory}/data/release_3.0.2/NOC_corrections/v1x2022/
+export corrections_directory=/ichec/work/glamod/data/marine/release_5.1/NOC_corrections/
 export jobs_directory=${code_directory}/config/jobs2.json
-export working_directory=/gws/nopw/j04/glamod_marine/working/r3.0.2/qc-suite/
+export working_directory=/ichec/work/glamod/glamod-marine-processing.2022/working/qc-suite/
+#/gws/nopw/j04/glamod_marine/working/r3.0.2/qc-suite/
 
 echo 'Code directory is '$code_directory
 echo 'Data directory is '$data_directory
@@ -18,13 +22,13 @@ echo 'Jobs directory is '$jobs_directory
 echo 'Working directory is '$working_directory
 
 # Activate python environment and add jaspy3.7 path to LD_LIBRARY_PATH so that cartopy and other can find the geos library
-source $pyEnvironment_directory/bin/activate
+source activate $pyEnvironment_directory
 export PYTHONPATH="$modules_directory:${PYTHONPATH}"
-export LD_LIBRARY_PATH=/apps/contrib/jaspy/miniconda_envs/jaspy3.7/m3-4.5.11/envs/jaspy3.7-m3-4.5.11-r20181219/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/ichec/packages/conda/2/lib/:$LD_LIBRARY_PATH
 echo "Python environment loaded from gws: $pyEnvironment_directory"
 
 # Create the scratch directory for the user
-scratch_directory=/work/scratch-nopw/$(whoami)
+scratch_directory=/ichec/work/glamod/$(whoami)
 if [ ! -d $scratch_directory ]
 then
   echo "Creating user $(whoami) scratch directory $scratch_directory"
