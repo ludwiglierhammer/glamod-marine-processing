502781822ba9247a0a7a70c1872273b69fa34358
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-04-23 10:43:28 +0200

    skeleton of python package

---
+++ glamod_marine_processing/__init__.py
@@ -0,0 +1,7 @@
+"""GLAMOD marine processing package."""
+from __future__ import annotations
+
+from . import config_suite
+from . import metadata_suite
+from . import obs_suite
+from . import qc_suite
