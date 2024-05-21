c5ab982c6799094745ead6f68af5b4c3f3662972
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-05-21 08:30:41 +0200

    make release source tree to cli; new CLI option: use already existing configuration file

--- glamod_marine_processing/cli.py
+++ glamod_marine_processing/cli.py
@@ -6,9 +6,59 @@ Marine processing Command Line Interface
 
 from __future__ import annotations
 
+import os
+
 import click
 
+from .utilities import load_json, mkdir
+
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"], show_default=True)
+PERIODS_FILE = "source_deck_periods.json"
+level_subdirs = {
+    "level1a": ["log", "quicklooks", "invalid", "excluded"],
+    "level1b": ["log", "quicklooks"],
+    "level1c": ["log", "quicklooks", "invalid"],
+    "level1d": ["log", "quicklooks"],
+    "level1e": ["log", "quicklooks", "reports"],
+    "level2": ["log", "quicklooks", "excluded", "reports"],
+}
+
+
+def make_release_source_tree(
+    data_path=None,
+    config_path=None,
+    release=None,
+    update=None,
+    dataset=None,
+    level=None,
+):
+    """Make release source tree."""
+
+    # Functions -------------------------------------------------------------------
+    def create_subdir(lpath, subdir_list):
+        """Create subdirectories."""
+        subdir_list = [subdir_list] if isinstance(subdir_list, str) else subdir_list
+        for subdir in subdir_list:
+            sub_dir = os.path.join(lpath, subdir)
+            mkdir(sub_dir)
+
+    # Go --------------------------------------------------------------------------
+    os.umask(0)
+    # READ LIST OF SID-DCKS FOR RELEASE
+    release_tag = "-".join([release, update])
+    release_periods_file = os.path.join(config_path, release_tag, dataset, PERIODS_FILE)
+    sid_dck_dict = load_json(release_periods_file)
+
+    level_subdirs_ = level_subdirs[level]
+    level_subdirs_.extend(".")
+
+    # CREATE DIRS
+    release_path = os.path.join(data_path, release)
+    source_path = os.path.join(release_path, dataset)
+    level_subdir = os.path.join(source_path, level)
+
+    for sublevel in level_subdirs_:
+        create_subdir(os.path.join(level_subdir, sublevel), sid_dck_dict.keys())
 
 
 def add_options():
@@ -80,13 +130,13 @@ class Options:
         self.data_directory = click.option(
             "-data_dir",
             "--data_directory",
-            help="Directory path of the input and output datasets. By default, take directory path from machine-depending configuration file."        
-        )    
+            help="Directory path of the input and output datasets. By default, take directory path from machine-depending configuration file.",
+        )
         self.work_directory = click.option(
             "-work_dir",
             "--work_directory",
-            help="Directory path of the log and template files. By default, take directory path from machine-depending configuration file."        
-        )             
+            help="Directory path of the log and template files. By default, take directory path from machine-depending configuration file.",
+        )
         self.split_files = click.option(
             "-split",
             "--split_files",
@@ -108,5 +158,12 @@ class Options:
             default="v1x2023",
             help="Name of the NOC corrections version.",
         )
+        self.config_file = click.option(
+            "-cfg",
+            "--config_file",
+            default=False,
+            help="Use already existing configuration file.",
+        )
+
 
 Options = Options()
