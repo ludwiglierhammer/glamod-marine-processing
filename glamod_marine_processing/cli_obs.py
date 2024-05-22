0e947422348e76ab5f51eb185d216c9fe979b839
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-05-22 07:15:07 +0200

    use genral command line interface class

--- glamod_marine_processing/cli_obs.py
+++ glamod_marine_processing/cli_obs.py
@@ -11,16 +11,8 @@ import shutil
 
 import click
 
-from .cli import CONTEXT_SETTINGS, add_options
-from .obs_suite.scripts.make_release_source_tree import make_release_source_tree
-from .utilities import (
-    add_to_config,
-    get_base_path,
-    get_configuration,
-    load_json,
-    mkdir,
-    save_json,
-)
+from .cli import CONTEXT_SETTINGS, Cli, add_options
+from .utilities import add_to_config, load_json, mkdir, save_json
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
@@ -33,54 +25,26 @@ def ObsCli(
     dataset,
     data_directory,
     work_directory,
+    config_file,
     submit_jobs,
 ):
     """Enry point for the obs_suite command line interface."""
-    release_update = f"{release}-{update}"
-    config = get_configuration(machine)
-    config["abbreviations"] = {
-        "release": release,
-        "update": update,
-        "dataset": dataset,
-        "release_tag": release_update,
-    }
-    if data_directory is not None:
-        config["paths"]["data_directory"] = data_directory
-    if work_directory is not None:
-        config["paths"]["glamod"] = work_directory
-
-    home_directory = get_base_path()
-    data_directory = config["paths"]["data_directory"]
-    code_directory = os.path.join(home_directory, "obs_suite")
-    config_directory = os.path.join(code_directory, "configuration_files")
-    config_files_path = os.path.join(config_directory, release_update, dataset)
-    scripts_directory = os.path.join(code_directory, "scripts")
-    lotus_scripts_directory = os.path.join(code_directory, "lotus_scripts")
-    work_directory = os.path.abspath(config["paths"]["glamod"])
-    scratch_directory = os.path.join(work_directory, os.getlogin())
-    release_directory = os.path.join(scratch_directory, release, dataset, level)
-
-    config = add_to_config(
-        config,
-        home_directory=home_directory,
-        code_directory=code_directory,
-        config_directory=config_directory,
-        config_files_path=config_files_path,
-        scripts_directory=scripts_directory,
-        lotus_scripts_directory=lotus_scripts_directory,
-        scratch_directory=scratch_directory,
-        release_directory=release_directory,
-        key="paths",
-    )
-
-    make_release_source_tree(
-        data_path=data_directory,
-        config_path=config_directory,
+    config = Cli(
+        machine=machine,
+        level=level,
         release=release,
         update=update,
         dataset=dataset,
-        level=level,
-    )
+        data_directory=data_directory,
+        work_directory=work_directory,
+        config_file=config_file,
+        suite="obs_suite",
+    ).initialize()
+
+    paths = config["paths"]
+    lotus_scripts_directory = paths["lotus_scripts_directory"]
+    release_directory = paths["release_directory"]
+    config_files_path = paths["config_files_path"]
 
     slurm_script = "level_slurm.py"
     slurm_script_ = f"{level}_slurm.py"
