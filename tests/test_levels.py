8e8dedb6832cfa27c18519d4c4b64111dad06810
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-06-05 11:19:39 +0200

    add skeleton level testing suite

---
+++ tests/test_levels.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import cdm_reader_mapper as cdm  # noqa
+import pytest  # noqa
+
+import glamod_marine_processing  # noqa
+
+
+def test_level1a():
+    """Testing level1a."""
+
+
+def test_level1b():
+    """Testing level1b."""
+
+
+def test_level1c():
+    """Testing level1c."""
+
+
+def test_level1d():
+    """Testing level1d."""
+
+
+def test_level1e():
+    """Testing level1e."""
+
+
+def test_level2():
+    """Testing level2."""
