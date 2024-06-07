bd140b08478d5692be6a1b2a2b9b975d5f9a5942
ludwig.lierhammer <ludwig.lierhammer@dwd.de>
2024-06-07 13:34:03 +0100

    add level1a testing suite

--- tests/test_obssuite.py
+++ tests/test_obssuite.py
@@ -1,42 +1,38 @@
 from __future__ import annotations
 
 import os
-
 import subprocess
 
-import cdm_reader_mapper as cdm  # noqa
+import cdm_reader_mapper as cdm
 import pytest  # noqa
-
-import glamod_marine_processing  # noqa
-from glamod_marine_processing.cli_obs import ObsCli
-
+from cdm_reader_mapper.common.getting_files import load_file
 from click.testing import CliRunner
 
-from cdm_reader_mapper.common.getting_files import load_file
+import glamod_marine_processing
+from glamod_marine_processing.cli_obs import ObsCli
 
-def test_level1a():
+
+def test_level1a_NRT():
     """Testing level1a."""
     load_file(
-        f"imma1_992/input/114-992_2016-01-01_subset.imma",
-        cache_dir=f"./datasets/ICOADS_R3.0.0T/level0/114-992",
+        f"imma1_992/input/114-992_2022-01-01_subset.imma",
+        cache_dir=f"./datasets/ICOADS_R3.0.2T/level0/114-992",
         within_drs=False,
     )
-    #runner = CliRunner()
-    #result = runner.invoke(ObsCli, ["-l level1a -r release_test -d ICOADS_R3.0.0T -data_dir . -work_dir ."])
-    #result = runner.run(ObsCli, ["-l level1a -r release_test -d ICOADS_R3.0.0T -data_dir . -work_dir ."])
-    #result = runner.invoke(ObsCli, ['-h'])
-    #print(result.output)
-    #print(result.stderr)
-    #print(result.stdout)
-    #subprocess.run(["obs_suite -l level1a -r release_test -d ICOADS_R3.0.0T -data_dir . -work_dir ."], shell=True)
-    subprocess.run("obs_suite -h", shell=True, check=True, capture_output=True)
-test_level1a()    
-#def test_level1a_NRT():
-#    """Testing level1a."""
-#    load_file(
-#        f"{dm}_{deck}/input/{data_file}",
-#        cache_dir=f"./datasets/ICOADS_R3.0.2T/level0/171-711",
-#    )
+    s = (
+        "obs_suite "
+        "-l level1a "
+        "-data_dir . "
+        "-work_dir . "
+        "-sp ???-???_????-??-??_subset.imma "
+        "-o "
+        "-run"
+    )
+    os.system(s)
+
+
+test_level1a_NRT()
+
 
 def test_level1b():
     """Testing level1b."""
