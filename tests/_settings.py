bf1820f93ec57799c2b56047b1912c905be20c66
ludwig.lierhammer <ludwig.lierhammer@dwd.de>
2024-06-13 08:00:09 +0100

    generalize testing suite

---
+++ tests/_settings.py
@@ -0,0 +1,139 @@
+from __future__ import annotations
+
+import numpy as np
+
+table_names = [
+    "header",
+    "observations-at",
+    "observations-dpt",
+    "observations-slp",
+    "observations-sst",
+    "observations-wbt",
+    "observations-wd",
+    "observations-ws",
+]
+
+table_names_1b = [
+    "header",
+    "observations-at",
+    "observations-dpt",
+    "observations-slp",
+    "observations-sst",
+    "observations-wd",
+    "observations-ws",
+]
+
+prev_level = {
+    "level1a": "level0",
+    "level1b": "level1a",
+    "level1c": "level1b",
+    "level1d": "level1c",
+    "level1e": "level1d",
+    "level2": "level1e",
+}
+
+level_input = {
+    "level1a": "datasets",
+    "level1b": "release_7.0",
+    "level1c": "release_7.0",
+    "level1d": "release_7.0",
+    "level1e": "release_7.0",
+    "level2": "release_7.0",
+}
+
+which_tables = {
+    "level1a": table_names,
+    "level1b": table_names_1b,
+    "level1c": ["header"],
+    "level1d": table_names_1b,
+    "level1e": table_names_1b,
+    "level2": table_names_1b,
+}
+
+pattern = {
+    "level1a": "???-???_????-??-??_subset.imma",
+    "level1b": "header-???-???_????-??-??_subset.psv",
+    "level1c": "header-???-???_????-??-??_subset.psv",
+    "level1d": "header-???-???_????-??-??_subset.psv",
+    "level1e": "header-???-???_????-??-??_subset.psv",
+    "level2": "header-???-???_????-??-??_subset.psv",
+}
+
+deletions = {
+    "level1a": [("header", "record_timestamp"), ("header", "history")],
+    "level1b": [("header", "record_timestamp"), ("header", "history")],
+    "level1c": ["record_timestamp", "history"],
+    "level1d": [("header", "record_timestamp"), ("header", "history")],
+    "level1e": [("header", "record_timestamp"), ("header", "history")],
+    "level2": [("header", "record_timestamp"), ("header", "history")],
+}
+
+manipulation = {
+    "level1a": {},
+    "level1b": {},
+    "level1c": {},
+    "level1d": {
+        ("header", "station_name"): [
+            "null",
+            "FF HELMER HANSEN",
+            "WAVERIDER TFSTD",
+            "NORNE",
+            "WAVERIDER TFDRN",
+        ],
+        ("header", "platform_sub_type"): ["null", "RV", "OT", "MI", "OT"],
+        ("header", "station_record_number"): ["1", "1", "0", "13", "0"],
+        ("header", "report_duration"): ["11", "HLY", "11", "HLY", "11"],
+        ("observations-at", "sensor_id"): ["null", "AT", np.nan, "null", np.nan],
+        ("observations-dpt", "sensor_id"): [np.nan, "HUM", np.nan, "null", np.nan],
+        ("observations-slp", "sensor_id"): ["null", "SLP", np.nan, "null", np.nan],
+        ("observations-sst", "sensor_id"): ["null", "SST", np.nan, np.nan, np.nan],
+        ("observations-wd", "sensor_id"): ["null", "WSPD", np.nan, "null", np.nan],
+        ("observations-ws", "sensor_id"): ["null", "WSPD", np.nan, "null", np.nan],
+        ("observations-at", "sensor_automation_status"): [
+            "5",
+            "3",
+            np.nan,
+            "5",
+            np.nan,
+        ],
+        ("observations-dpt", "sensor_automation_status"): [
+            np.nan,
+            "3",
+            np.nan,
+            "5",
+            np.nan,
+        ],
+        ("observations-slp", "sensor_automation_status"): [
+            "5",
+            "3",
+            np.nan,
+            "5",
+            np.nan,
+        ],
+        ("observations-sst", "sensor_automation_status"): [
+            "5",
+            "3",
+            np.nan,
+            np.nan,
+            np.nan,
+        ],
+        ("observations-wd", "sensor_automation_status"): [
+            "5",
+            "3",
+            np.nan,
+            "5",
+            np.nan,
+        ],
+        ("observations-ws", "sensor_automation_status"): [
+            "5",
+            "3",
+            np.nan,
+            "5",
+            np.nan,
+        ],
+    },
+    "level1e": {
+        ("header", "report_quality"): ["2", "2", "2", "2", "2"],
+    },
+    "level2": {},
+}
