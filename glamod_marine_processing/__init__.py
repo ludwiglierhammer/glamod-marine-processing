7c4929345d8327b9fafb9ab9b8a0b8d590b1fef4
ludwig.lierhammer <ludwig.lierhammer@dwd.de>
2024-06-14 08:39:26 +0100

    delete metadata_suite and config_suite

--- glamod_marine_processing/__init__.py
+++ glamod_marine_processing/__init__.py
@@ -1,4 +1,4 @@
 """GLAMOD marine processing package."""
 from __future__ import annotations
 
-from . import config_suite, metadata_suite, obs_suite, preprocessing, qc_suite  # noqa
+from . import obs_suite, qc_suite  # noqa
