7a7fae249dd54bc6f0e552e6fb4c9a557fd5f513
ludwig.lierhammer <ludwig.lierhammer@dwd.de>
2024-06-14 08:33:17 +0100

    not used: delete config suite

--- glamod_marine_processing/config_suite/__init__.py
+++
@@ -1,2 +0,0 @@
-"""GLAMOD marine processing config suite package."""
-from __future__ import annotations
--- glamod_marine_processing/config_suite/modules/imma_noc.py
+++
@@ -1,2 +0,0 @@
-"""GLAMOD marine processing pre-processing package."""
-from __future__ import annotations
