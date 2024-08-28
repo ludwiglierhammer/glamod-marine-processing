bdc6fea93b457c45ec7e9e44f5cfc14ce196f5be
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-08-28 09:00:40 +0200

    import pre-processing

--- glamod_marine_processing/__init__.py
+++ glamod_marine_processing/__init__.py
@@ -3,3 +3,4 @@
 from __future__ import annotations
 
 from . import obs_suite, qc_suite  # noqa
+from .pre_processing import pre_processing  # noqa
--- glamod_marine_processing/pre_processing/__init__.py
+++ glamod_marine_processing/pre_processing/__init__.py
@@ -1,3 +1,5 @@
 """GLAMOD marine processing pre-processing package."""
 
 from __future__ import annotations
+
+from .pre_processing import pre_processing  # noqa
