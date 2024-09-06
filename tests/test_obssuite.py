e5d7127906040f212e984910b2ef76909c9eebf6
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-09-06 10:28:19 +0200

    run C-RAID tests

--- tests/test_obssuite.py
+++ tests/test_obssuite.py
@@ -4,11 +4,9 @@ import pytest
 from _testing_suite import _obs_testing
 
 
-# @pytest.mark.parametrize("dataset", ["C-RAID_1.2", "ICOADS_R3.0.2T"])
-# @pytest.mark.parametrize(
-#    "level", ["level1a", "level1b", "level1c", "level1d", "level1e", "level2"]
-# )
-@pytest.mark.parametrize("dataset", ["ICOADS_R3.0.2T"])
-@pytest.mark.parametrize("level", ["level1a"])
+@pytest.mark.parametrize("dataset", ["C-RAID_1.2", "ICOADS_R3.0.2T"])
+@pytest.mark.parametrize(
+    "level", ["level1a", "level1b", "level1c", "level1d", "level1e", "level2"]
+)
 def test_levels(capsys, dataset, level):
     _obs_testing(dataset, level, capsys)
