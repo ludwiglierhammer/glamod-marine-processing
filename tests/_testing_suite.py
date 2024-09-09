b451d08d2403f64e0fac9bfe9ced7583150e0c10
ludwiglierhammer <ludwig.lierhammer@dwd.de>
2024-09-09 09:10:13 +0200

    comment p_id

--- tests/_testing_suite.py
+++ tests/_testing_suite.py
@@ -48,10 +48,11 @@ def _obs_testing(dataset, level, capsys):
         f"-data_dir ./T{level} "
         f"-work_dir ./T{level} "
         f"-sp {_settings.pattern[level]} "
-        "-p_id {_settings.p_id} "
+        # "-p_id {_settings.p_id} "
         "-o "
         "-run"
     )
+
     os.system(s)
     captured = capsys.readouterr()
     assert captured.out == ""
