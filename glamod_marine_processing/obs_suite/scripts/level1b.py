bbc1ee6e5d6b014fefb50f6aacebe82484008ea6
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-10-09 11:55:17 +0200

    C-RAID_ ignore primary_station_id during duplicate check in level1b

--- glamod_marine_processing/obs_suite/scripts/level1b.py
+++ glamod_marine_processing/obs_suite/scripts/level1b.py
@@ -88,7 +88,7 @@ else:
     logging.error("Need arguments to run!")
     sys.exit(1)
 
-process_options = ["correction_version", "corrections", "histories"]
+process_options = ["correction_version", "corrections", "histories", "ignore_columns"]
 params = script_setup(process_options, args, "level1b", "level1a")
 
 cor_ext = ".txt.gz"
@@ -213,7 +213,9 @@ for table in cdm.properties.cdm_tables:
     if table == "header":
         correction_dict["duplicates"] = {}
         if params.correction_version == "null":
-            DupDetect = cdm.duplicate_check(table_df)
+            DupDetect = cdm.duplicate_check(
+                table_df, ignore_columns=params.ignore_columns
+            )
             DupDetect.flag_duplicates()
             table_df = DupDetect.result
         contains_info = table_df["duplicate_status"] != dupNotEval
