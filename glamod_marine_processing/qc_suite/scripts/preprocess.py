cccbfd58b37c8b68fb4cd6e13f89aee6c9ea4105
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-10-18 10:46:18 +0200

    replace deprecated DataFrame.append with DataFrame.merge

--- glamod_marine_processing/qc_suite/scripts/preprocess.py
+++ glamod_marine_processing/qc_suite/scripts/preprocess.py
@@ -468,7 +468,7 @@ for yr in range(y_init - 1, y_end + 2):
                     drifters["UID"] = prepend + drifters.UID
                     drifters = drifters.set_index("UID")
                     # add to data dataframe
-                    data_dl = data_dl.append(drifters)
+                    data_dl = data_dl = data_dl.merge(drifters, on="UID", how="left")
 
             if data_dl.empty:
                 print("Dataframe is empty. Skipping")
