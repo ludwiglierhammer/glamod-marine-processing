852efd5d53b8cae71ce340161466ee2f2bb097c3
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-10-23 17:39:52 +0200

    run 12 jobs in parallel

--- glamod_marine_processing/obs_suite/lotus_scripts/level_slurm.py
+++ glamod_marine_processing/obs_suite/lotus_scripts/level_slurm.py
@@ -247,5 +247,5 @@ for sid_dck in process_list:
         elif script_config["parallel_jobs"] is True:
             logging.info("Run jobs interactively in parallel.")
             subprocess.call(
-                ["/bin/parallel", "--jobs", "10", "::::", taskfarm_file], shell=False
+                ["/bin/parallel", "--jobs", "12", "::::", taskfarm_file], shell=False
             )
