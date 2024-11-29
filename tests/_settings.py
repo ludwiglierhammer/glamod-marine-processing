86b3ffa8e48001709253b593c2f1c4b4844cf1a2
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2024-11-29 14:07:00 +0100

    typo

--- tests/_settings.py
+++ tests/_settings.py
@@ -5,7 +5,7 @@ def get_settings(dataset):
     if dataset == "C-RAID_1.2":
         import _settings_CRAID12
 
-        return _settings_CRAID
+        return _settings_CRAID12
     if dataset == "ICOADS_R3.0.2T":
         import _settings_ICOADS302
 
