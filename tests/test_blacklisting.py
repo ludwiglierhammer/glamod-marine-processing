3230d24e8195fe3e02873962c45a03f10e2cdcd4
Ludwig Lierhammer <ludwig.lierhammer@dwd.de>
2025-05-28 11:08:28 +0200

    update assertions

--- tests/test_blacklisting.py
+++ tests/test_blacklisting.py
@@ -2,24 +2,23 @@ from __future__ import annotations
 
 import pytest
 
-from glamod_marine_processing.qc_suite.modules.blacklisting import (
+from glamod_marine_processing.obs_suite.modules.blacklisting import (
     do_blacklist,
     do_humidity_blacklist,
     do_mat_blacklist,
     do_wind_blacklist,
 )
-from glamod_marine_processing.qc_suite.modules.qc import failed, passed
 
 
 @pytest.mark.parametrize(
     "id, deck, year, month, latitude, longitude, platform_type, expected",
     [
-        ("", 980, 1850, 1, 0, 0, 1, failed),  # fails lat/lon = 0 zero check
-        ("", 874, 1850, 1, 0, 1, 1, failed),  # Deck 874 SEAS fail
-        ("", 732, 1850, 1, 0, 1, 13, failed),  # C-MAN station fail
-        ("", 732, 1850, 1, 0, 1, 1, passed),  # Deck 732 pass
-        ("", 732, 1958, 1, 45, -172, 1, failed),  # Deck 732 fail
-        ("", 732, 1974, 1, -47, -60, 1, failed),  # Deck 732 fail
+        ("", 980, 1850, 1, 0, 0, 1, False),  # fails lat/lon = 0 zero check
+        ("", 874, 1850, 1, 0, 1, 1, False),  # Deck 874 SEAS fail
+        ("", 732, 1850, 1, 0, 1, 13, False),  # C-MAN station fail
+        ("", 732, 1850, 1, 0, 1, 1, True),  # Deck 732 pass
+        ("", 732, 1958, 1, 45, -172, 1, False),  # Deck 732 fail
+        ("", 732, 1974, 1, -47, -60, 1, False),  # Deck 732 fail
         (
             "",
             732,
@@ -28,7 +27,7 @@ from glamod_marine_processing.qc_suite.modules.qc import failed, passed
             45,
             -172 + 360,
             1,
-            failed,
+            False,
         ),  # Deck 732 shifted longitude fail
         (
             "",
@@ -38,7 +37,7 @@ from glamod_marine_processing.qc_suite.modules.qc import failed, passed
             -47,
             -60 + 360,
             1,
-            failed,
+            False,
         ),  # Deck 732 shifted longitude fail
         (
             "",
@@ -48,7 +47,7 @@ from glamod_marine_processing.qc_suite.modules.qc import failed, passed
             45,
             -172,
             1,
-            passed,
+            True,
         ),  # Same are but not in Deck 732 should pass
         (
             "",
@@ -58,7 +57,7 @@ from glamod_marine_processing.qc_suite.modules.qc import failed, passed
             -47,
             -60,
             1,
-            passed,
+            True,
         ),  # Same are but not in Deck 732 should pass
         (
             "",
@@ -68,7 +67,7 @@ from glamod_marine_processing.qc_suite.modules.qc import failed, passed
             45,
             -172,
             1,
-            passed,
+            True,
         ),  # Same area but wrong year should pass
         (
             "",
@@ -78,11 +77,11 @@ from glamod_marine_processing.qc_suite.modules.qc import failed, passed
             -47,
             -60,
             1,
-            passed,
+            True,
         ),  # Same area but wrong year should pass
-        ("SUPERIGORINA", 162, 1999, 2, -10, 179, 4, failed),
-        ("53521    ", 162, 2005, 11, -10, 179, 4, failed),
-        ("53521    ", 162, 2007, 11, -10, 179, 4, passed),
+        ("SUPERIGORINA", 162, 1999, 2, -10, 179, 4, False),
+        ("53521    ", 162, 2005, 11, -10, 179, 4, False),
+        ("53521    ", 162, 2007, 11, -10, 179, 4, True),
     ],
 )
 def test_do_blacklist(
@@ -96,9 +95,9 @@ def test_do_humidity_blacklist():
     for platform_type in range(0, 47):
         result = do_humidity_blacklist(platform_type)
         if platform_type in [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 15]:
-            assert result == passed
+            assert result is True
         else:
-            assert result == failed
+            assert result is False
 
 
 @pytest.mark.parametrize(
@@ -110,25 +109,25 @@ def test_do_humidity_blacklist():
             0.5,
             2.0,
             2011,
-            1,
+            False,
         ),  # Check Deck 780 platform type 5 combination that fails
-        (5, 781, 0.5, 2.0, 2011, passed),  # and variants that should pass
-        (6, 780, 0.5, 2.0, 2011, passed),  # and variants that should pass
-        (1, 193, 45.0, -40.0, 1885, failed),  # In the exclusion zone
-        (1, 193, 25.0, -40.0, 1885, passed),  # Outside the exclusion zone (in space)
-        (1, 193, 45.0, -40.0, 1877, passed),  # Outside the exclusion zone (in time)
-        (1, 193, 45.0, -40.0, 1999, passed),  # Outside the exclusion zone (in time)
+        (5, 781, 0.5, 2.0, 2011, True),  # and variants that should pass
+        (6, 780, 0.5, 2.0, 2011, True),  # and variants that should pass
+        (1, 193, 45.0, -40.0, 1885, False),  # In the exclusion zone
+        (1, 193, 25.0, -40.0, 1885, True),  # Outside the exclusion zone (in space)
+        (1, 193, 45.0, -40.0, 1877, True),  # Outside the exclusion zone (in time)
+        (1, 193, 45.0, -40.0, 1999, True),  # Outside the exclusion zone (in time)
     ],
 )
 def test_do_mat_blacklist(platform_type, deck, latitude, longitude, year, expected):
     result = do_mat_blacklist(platform_type, deck, latitude, longitude, year)
-    assert result == expected
+    assert result is expected
 
 
 def test_do_wind_blacklist():
     for deck in range(1, 1000):
         result = do_wind_blacklist(deck)
         if deck in [708, 780]:
-            assert result == failed
+            assert result is False
         else:
-            assert result == passed
+            assert result is True
